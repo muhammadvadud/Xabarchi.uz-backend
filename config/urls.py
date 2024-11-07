2e0ef01de991083614f397fe00f9b8392afa5797
muhammadvadud <gmuhammadvadud@gmail.com>
2024-11-07 16:00:29 +0500

    bug fix

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/v1/', include('api.urls'))
+    path('api/v1/', include('api.urls')),
+
+    re_path(r"media/(.*)", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"static/(.*)", serve, {"document_root": settings.STATIC_ROOT})
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
